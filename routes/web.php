7c3aaca1b6d59c1a5fef8782e7217132b11b1b85
samra <samrasagheer165@gmail.com>
2022-11-18 16:47:25 +0500

    view change

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\servicesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('sam',[servicesController::class,'index']);
